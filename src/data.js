8361324fcd60231334a0770615a4a04401eaaeac
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-21 17:39:04 -0300

    filtragem por nome de a à  z

--- src/data.js
+++ src/data.js
@@ -1,23 +1,38 @@
 // utilizei o filter pois ele retorna todos os elementos do array
 
-
-export const filterPokemon = (listPokemon, pokemonInfo) => {
+export const filterData = (listPokemon, pokemonInfo) => {
   const newListPokemon = listPokemon.filter(function (pokemon) {
-    debugger
     return pokemon.name === pokemonInfo;
   })
   return newListPokemon;
 };
 
-console.log()
 
-//então quando chamar o filterPokemon no main.js, ele vai renderizar TODAS as informações
-//mas como deixei aparecer só 4 delas, irei selecionar qual irá aparecer
+export const sortData = (data, infoBy, infoOrder) => {
+    if(infoOrder === "A-Z"){
+      data.sort(function(a, b){
+        if (a[infoBy] > b[infoBy]) {
+          return 1;
+        }
+        if (a[infoBy] < b[infoBy]) {
+          return -1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+    } else if(infoOrder === "Z-A"){
+      data.sort(function(a, b){
+        if (a[infoBy] < b[infoBy]) {
+          return 1;
+        }
+        if (a[infoBy] > b[infoBy]) {
+          return -1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+    }
+  return data;
+};
 
-
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
- 
 
