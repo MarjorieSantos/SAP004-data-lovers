14f739aa773088c15b7b98174a1c02c03461ecbe
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-20 23:47:32 -0300

    inclusão do array filter

--- src/data.js
+++ src/data.js
@@ -1,10 +1,23 @@
-// funções de exemplo
+// utilizei o filter pois ele retorna todos os elementos do array
 
-export const example = () => {
-  return 'example';
+
+export const filterPokemon = (listPokemon, pokemonInfo) => {
+  const newListPokemon = listPokemon.filter(function (pokemon) {
+    debugger
+    return pokemon.name === pokemonInfo;
+  })
+  return newListPokemon;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+console.log()
+
+//então quando chamar o filterPokemon no main.js, ele vai renderizar TODAS as informações
+//mas como deixei aparecer só 4 delas, irei selecionar qual irá aparecer
+
+
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+ 
 
