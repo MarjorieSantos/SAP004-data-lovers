03a64d311c0df3177d8f246c1f795e788b84ccb9
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-21 20:07:25 -0300

    render na tela de acordo com o filtro escolhido

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import data from './data/pokemon/pokemon.js';
 const listCards = document.querySelector("#list-cards")
 
 function render(arrayPokemon) {
+  listCards.innerHTML = ""
   arrayPokemon.forEach(function (pokemon) {
     listCards.innerHTML += `
         <div class="cards">
@@ -22,14 +23,27 @@ function render(arrayPokemon) {
 
 render(data.pokemon)
 
-filterData(data.pokemon, "Bulbasaur")
+// filterData(data.pokemon, "Bulbasaur")
 
 
 
 const filter = document.querySelector("#filter")
 
 filter.addEventListener("change", function (event) {
-  
+  if (filter.value == "A-Z") {
+    const arrayOrder = sortData(data.pokemon, "name", "A-Z")
+    render(arrayOrder)
+  } else if (filter.value == "Z-A") {
+    const arrayOrder = sortData(data.pokemon, "name", "Z-A")
+    render(arrayOrder)
+  } else if (filter.value == "ascNum") {
+    const arrayOrder = sortData(data.pokemon, "num", "ascNum")
+    render(arrayOrder)
+  } else if (filter.value == "descNum") {
+    const arrayOrder = sortData(data.pokemon, "num", "descNum")
+    render(arrayOrder)
+  }
+
 })
 
 
