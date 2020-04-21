c5c5f41b45b9cd8aae6cdc41aab6ec7b0f798319
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-21 18:55:01 -0300

    adicionando evento click no select

--- src/main.js
+++ src/main.js
@@ -2,15 +2,8 @@
 import { filterData, sortData } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-//#list-cards vem da section do HTML que agrupa todos os cards que irão aparecer na tela, assim guardei ele em uma variável chamada ListCard
 const listCards = document.querySelector("#list-cards")
 
-//para não ter que ficar criando vários innerHTML, a função render recebe 
-//um array de todos os pokemons que está no arquivo pokemon.js
-//listCard irá receber cada card a partir da execução do forEach  
-//o forEach() --> vai percorrer o array:pokemon até o final, quando terminar vai printar no HTML as informações de cada personagem
-
-//tamplateString "inclui" a div(cards) na section list-cards do HTML
 function render(arrayPokemon) {
   arrayPokemon.forEach(function (pokemon) {
     listCards.innerHTML += `
@@ -19,21 +12,24 @@ function render(arrayPokemon) {
             <ul>
               <li>Nome: ${pokemon.name}</li>
               <li>Nº: ${pokemon.num}</li>
-              <li>Tipo:</li>
-              <li>Poder:</li>
+              <li>Tipo: ${pokemon.type}</li>
+              <li>Poder: </li>
             </ul>
           </div>
         `
   })
 }
-//para que a inforamação apareça no local "correto", basta concatenar 
-//com o item do objeto que você quer  ex: ${pokemon.name} -> vai pegar apenas o NOME do pokemon e mostrar no item "nome", como se fosse um span
 
-//o render sempre vai receber um array de "pokemon" - data
-// e irá mostrar na tela os cards de acordo com o array que foi passado
 render(data.pokemon)
 
 filterData(data.pokemon, "Bulbasaur")
 
-sortData(data.pokemon, "name", "Z-A")
+
+
+const filter = document.querySelector("#filter")
+
+filter.addEventListener("change", function (event) {
+  
+})
+
 
