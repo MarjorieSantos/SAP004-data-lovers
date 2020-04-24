973f144159609d93665e13446b7610771847f6d7
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-04-24 16:55:23 -0300

    filtrando por nome  renderizando na tela

--- src/main.js
+++ src/main.js
@@ -2,14 +2,42 @@ import { filterByName } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
+const pokemons = data.pokemon
 
 const searchInput = document.getElementById("search-input")
+const pokemonList = document.getElementById("pokemon-list")
+const clearBtn = document.getElementById("clear-btn")
+const noResultFound = document.getElementById("no-result-found")
+
+
+const renderPokemon = (pokemon) => {
+    const template = document.getElementById("pokemon-template")
+    const card = template.content.querySelector(".card")
+    const pokemonName = card.querySelector(".name")
+    const pokemonImage = card.querySelector(".img")
+    const pokemonNumber = card.querySelector(".num")
+    const pokemonType = card.querySelector(".type")
+    pokemonName.textContent = pokemon.name
+    pokemonImage.setAttribute("src", pokemon.img)
+    pokemonNumber.textContent = pokemon.num 
+    pokemonType.textContent = pokemon.type
+    const templateDoned = document.importNode(template.content, true)
+    pokemonList.appendChild(templateDoned)
+}
+
+const clearPokemonList = () =>{
+    pokemonList.innerHTML = ""
+}
 
 
 const searchByName = () => {
-    const searchTherme = searchInput.value
-    const result = filterByName(data.pokemon,searchTherme)
-    console.log(result)
+    const searchTerm = searchInput.value
+    const result = filterByName(data.pokemon,searchTerm)
+    clearPokemonList()
+    result.forEach(renderPokemon)
 }
 
 searchInput.addEventListener("keyup",searchByName)
+clearBtn.addEventListener("click",clearPokemonList)
+
+
