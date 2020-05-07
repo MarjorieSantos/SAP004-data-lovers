a94de7f51dade16af2c952bd5330f4814af4f810
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-05-07 15:33:51 -0300

    tentative teste

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,48 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterByName, sortData, computeStats } from '../src/data.js';
+import data from '../src/data/pokemon/pokemon';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('filterByName', () => {
+  it('should be an function', () => {
+    expect(typeof filterByName).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('should throw TypeError when invoked with wrong argument types', () => {
+    expect(() => filterByName()).toThrow(TypeError);
+    expect(() => filterByName(0)).toThrow(TypeError);
+    expect(() => filterByName(null, [])).toThrow(TypeError);
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe('sortData', () => {
+  it('should be an function', () => {
+    expect(typeof sortData).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('should return "Abra" to "Zubat" with sort order "A-Z"', () => {
+    expect(sortData (data.pokemon, "name", "A-Z")[0].name).toBe('Abra');
+  });
+
+  it('should return "Zubat" to "Abra" with sort order "Z-A"', () => {
+    expect(sortData (data.pokemon, "name", "Z-A")[0].name).toBe('Zubat');
+  });
+
+  it('should return "Bulbasaur" to "Mew" with sort order "1-151"', () => {
+    expect(sortData (data.pokemon, "num", "crescentOrder")[0].name).toBe('Bulbasaur');
+  });
+
+  it('should return "Mew" to "Bulbasaur" with sort order "151-1"', () => {
+    expect(sortData (data.pokemon, "num", "decreasingOrder")[0].name).toBe('Mew');
+  });
+});
+
+describe('computeStats', () => {
+  it('should be an function', () => {
+    expect(typeof computeStats).toBe('function');
+  });
+
+  it('should return "16.6" for "6.9 kg" ', () => {
+    expect(computeStats(data.pokemon, {weight: "6.9 kg"})).toBe('16.6');
   });
 });
